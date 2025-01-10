e523d67206aa70936654bc89ddc0a40e300445eb
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-10 19:31:51 +0700

    Add a form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
 
   useEffect(() => {
     axios
@@ -11,13 +12,32 @@ const App = () => {
       .then((res) => setPersons(res.data));
   });
 
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    setNewName("");
+  };
+
   return (
     <>
-      <h2>Phonebook</h2>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <Entry key={person.id} person={person} />
-      ))}
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            placeholder="Enter a name..."
+            required
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+      <div>
+        <h2>Numbers</h2>
+        {persons.map((person) => (
+          <Entry key={person.id} person={person} />
+        ))}
+      </div>
     </>
   );
 };
