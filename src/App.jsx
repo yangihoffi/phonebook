5c557c899e090d73bc1b2e620db0f831290dcbd9
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-10 19:23:38 +0700

    Fetch dummy db and show names

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import axios from "axios";
+import { useState, useEffect, Fragment } from "react";
+
 const App = () => {
-  return <></>;
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data));
+  });
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <Fragment key={person.id}>
+          <p>{person.name}</p>
+          <br />
+        </Fragment>
+      ))}
+    </>
+  );
 };
 
 export default App;
